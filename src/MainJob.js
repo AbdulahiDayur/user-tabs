af710fc532fcf96adfd90460d638cf7d0a7fa56b
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-06-28 20:32:11 -0700

    removed comments

--- src/MainJob.js
+++ src/MainJob.js
@@ -25,29 +25,3 @@ function MainJob({ mainData, value }) {
 }
 
 export default MainJob;
-
-// return (
-//   <>
-//     {mainData.map((aJob, index) => {
-//       if (index < 1) {
-//         const { id, title, dates, duties, company } = aJob;
-//         return (
-//           <div key={id} className="job-info">
-//             <h3>{title}</h3>
-//             <h4>{company}</h4>
-//             <p className="job-date">{dates}</p>
-
-//             {duties.map((para, id) => {
-//               return (
-//                 <div key={id} className="job-desc">
-//                   <FaAngleDoubleRight className="job-icon" />
-//                   <p>{para}</p>
-//                 </div>
-//               );
-//             })}
-//           </div>
-//         );
-//       }
-//     })}
-//   </>
-// );
