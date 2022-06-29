0eb36b2d9c8b7d51d3ac9f1bee11d6e7d375e69a
AbdulahiDayur <Abdulahidayur@gmail.com>
2022-06-28 20:31:01 -0700

    structure fix + cleaner code

--- src/MainJob.js
+++ src/MainJob.js
@@ -1,32 +1,53 @@
 import React from "react";
 import { FaAngleDoubleRight } from "react-icons/fa";
 
-function MainJob({ mainData }) {
+function MainJob({ mainData, value }) {
+  const { title, dates, duties, company } = mainData[value];
+
   return (
     <>
-      {mainData.map((aJob, index) => {
-        if (index < 1) {
-          const { id, title, dates, duties, company } = aJob;
-          return (
-            <div key={id} className="job-info">
-              <h3>{title}</h3>
-              <h4>{company}</h4>
-              <p className="job-date">{dates}</p>
+      <div className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
 
-              {duties.map((para, id) => {
-                return (
-                  <div key={id} className="job-desc">
-                    <FaAngleDoubleRight className="job-icon" />
-                    <p>{para}</p>
-                  </div>
-                );
-              })}
+        {duties.map((para, id) => {
+          return (
+            <div key={id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon" />
+              <p>{para}</p>
             </div>
           );
-        }
-      })}
+        })}
+      </div>
     </>
   );
 }
 
 export default MainJob;
+
+// return (
+//   <>
+//     {mainData.map((aJob, index) => {
+//       if (index < 1) {
+//         const { id, title, dates, duties, company } = aJob;
+//         return (
+//           <div key={id} className="job-info">
+//             <h3>{title}</h3>
+//             <h4>{company}</h4>
+//             <p className="job-date">{dates}</p>
+
+//             {duties.map((para, id) => {
+//               return (
+//                 <div key={id} className="job-desc">
+//                   <FaAngleDoubleRight className="job-icon" />
+//                   <p>{para}</p>
+//                 </div>
+//               );
+//             })}
+//           </div>
+//         );
+//       }
+//     })}
+//   </>
+// );
